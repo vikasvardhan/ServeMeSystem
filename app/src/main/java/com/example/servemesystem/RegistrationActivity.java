3df07158931e43750edf849705d201a6d646c487
vikasvardhan <Vikas@230791>
2020-02-19 11:19:43 -0600

    Added consistent color theme

--- app/src/main/java/com/example/servemesystem/RegistrationActivity.java
+++ app/src/main/java/com/example/servemesystem/RegistrationActivity.java
@@ -61,7 +61,7 @@ public class RegistrationActivity extends AppCompatActivity {
                                 finish();
                                redirectLogin();
                             }
-                        }, 500);
+                        }, 1500);
                     }
                 }
             }
